ab709869b5bd016740fa7e1b2045d2757bfff820
barseghyanmher <barseghyanm@list.ru>
2020-12-03 12:56:57 +0400

    Homework 2 Exercise 6

---
+++ Homework_2/exercise_6.py
@@ -0,0 +1,20 @@
+number = int(input("Enter a pair number :"))
+num_list = [int(i) for i in range(1, number+1)]
+
+
+def prime(num):
+    prime_flag = True
+
+    for i in range(2, num // 2):
+        if num % i == 0:
+            prime_flag = False
+            break
+    return prime_flag
+
+
+for num1 in num_list[1:]:
+    if prime(num1):
+        num2 = number - num1
+        if prime(num2):
+            print(num1, num2)
+            break
