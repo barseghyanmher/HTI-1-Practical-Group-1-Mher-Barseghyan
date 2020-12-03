ecf6e71d36864b7f509fd333dcffdd04c9bee316
Mher <74491390+barseghyanmher@users.noreply.github.com>
2020-12-03 13:27:38 +0400

    Update exercise_6.py
    Add  "+ 1" in string number 8

--- Homework_2/exercise_6.py
+++ Homework_2/exercise_6.py
@@ -5,7 +5,7 @@ num_list = [int(i) for i in range(1, number+1)]
 def prime(num):
     prime_flag = True
 
-    for i in range(2, num // 2):
+    for i in range(2, num // 2 + 1):
         if num % i == 0:
             prime_flag = False
             break
