923e3499e51509d774b2e8dd7a8e587aaa5d1b94
barseghyanmher <barseghyanm@list.ru>
2020-12-09 20:47:18 +0400

    Changed

--- Homework_2/exercise_4.py
+++ Homework_2/exercise_4.py
@@ -5,14 +5,14 @@ main_list = [int(i) for i in number]
 def odd_indexs(lst):
     odd_indexs_lst = []
     for i in range(1, len(lst), 2):
-        odd_indexs_lst += [(lst[i])]
+        odd_indexs_lst += [lst[i]]
     return odd_indexs_lst
 
 
 def pair_indexs(lst):
     pair_indexs_lst = []
     for i in range(0, len(lst), 2):
-        pair_indexs_lst += [(lst[i])]
+        pair_indexs_lst += [lst[i]]
     return pair_indexs_lst
 
 
