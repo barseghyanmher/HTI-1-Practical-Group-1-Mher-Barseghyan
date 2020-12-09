c596bf585d404202d15b8ad3a6ccb80a2a21075f
barseghyanmher <barseghyanm@list.ru>
2020-12-09 20:53:10 +0400

    just

--- Homework_1/is_palindrome.py
+++ Homework_1/is_palindrome.py
@@ -1,5 +1,5 @@
-string=input(("Enter a letter:"))  
-if(string==string[::-1]):  
-      print("Yes")
-else:  
-      print("No")
+string = input("Enter a letter:")
+if string == string[::-1]:
+    print("Yes")
+else:
+    print("No")
