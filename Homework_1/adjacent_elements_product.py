619ae5b03edf650ef0b220167b6137e83b61392b
barseghyanmher <barseghyanm@list.ru>
2020-12-09 20:48:28 +0400

    Somethink

--- Homework_1/adjacent_elements_product.py
+++ Homework_1/adjacent_elements_product.py
@@ -1,7 +1,7 @@
-list1 = [input("Enter numbers with spaces and push 'Enter' :")]
-intlst = [int(n) for n in list1[0].split()]
-avg=[0]*len(intlst)
+list1 = input("Enter numbers with spaces and push 'Enter' :")
+intlst = [int(n) for n in list1.split()]
+avg = [0] * len(intlst)
 for i in range(len(intlst) - 1):
-    avg[i]=(intlst[i] * intlst[i + 1])
+    avg[i] = (intlst[i] * intlst[i + 1])
 maximum = max(avg)
 print(maximum)
